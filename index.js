f058353a6d2f666ada0c3091bee07886755a7c18
Amby <amby-navy@protonmail.com>
2022-07-03 19:50:56 +0100

    fucken

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 // Require the necessary discord.js classes
-const { Client, Intents, MessageEmbed } = require('discord.js');
+const { Client, Intents, MessageEmbed, Guild } = require('discord.js');
 const { token } = require('./config.json');
 const fs = require('fs')
 const prefix = "$";
-var unitauth = ['239986007313743873', '254216945375772673', '480792708462673920', '616316541499342848', '291933059576627200', '660046048667893760', '182162812276047874', '480088366663335938'];
-var adminauth = ['239986007313743873', '319658921126133762', '480792708462673920', '254216945375772673', '616316541499342848', '182162812276047874', '480088366663335938'];
-var mcauth = ['701517292990627881', '254216945375772673', '182162812276047874'];
-var ambyauth = "182162812276047874"
-const airole = "931138758718394408";
+var unitauth = ['239986007313743873', '254216945375772673', '480792708462673920', '616316541499342848', '291933059576627200', '660046048667893760', '182162812276047874', '480088366663335938', '291933059576627200', '701517292990627881'];
+var adminauth = ['239986007313743873', '319658921126133762', '480792708462673920', '254216945375772673', '616316541499342848', '182162812276047874', '480088366663335938', '291933059576627200'];
+var mcauth = ['701517292990627881', '254216945375772673', '182162812276047874','291933059576627200'];
+var munchauth = ["182162812276047874", "701517292990627881", "480792708462673920", "322850804761624576", "610736360411758604"]
+var ambyauth = "182162812276047874";
+var ambyaltauth = "962453954543902740";
+const airole = "982828262843617301";
 const munchrole = "966124012394655815";
 var bannednums = [88];
 var remchannels = [];
-
+var annoy = false;
+var muteeveryone = false;
+var tobeannoyed = 0;
+var wumbusannoy = 716860776694153296;
+var wumbusquiet = false;
+const jihadrole = "989562510023733319";
+var jihadauth = ['828555638837346354', '182162812276047874'];
+var bannedwords = ["cock", "vulva", "penile", "penes", "dick", "porn", "sex", "nigger", "faggot", "vagina", "penis", "nazi", "nigga", "furries", "furry", "yiff", "pubes", "pubic", "testicles", "nsfw", "boob", "breasts", "%46%6f%72%72%65%73 %41%63%61%64%65%6d%79", "semen"]; 
+const airole2 = '911164577071378493';
+const guesscooleddown = new Set();
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,  Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILDS] });
@@ -24,14 +35,26 @@ client.once('ready', () => {
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min +1) + min);
 }
+
 client.on("messageCreate", async (msg) => {
-    
-    if (remchannels.includes(msg.channel.id)) {
+    if (msg.author.id == wumbusannoy && wumbusquiet == true) {
         msg.delete();
-        
+    }
+    if (msg.author.id == tobeannoyed && annoy == true) {
+        var random = randomIntFromInterval(1, 3);
+        if (random == 3){
+            msg.delete();
+            console.log("deleted");
+        }
+    }
+    if (muteeveryone == true && remchannels.includes(msg.channel.id)) {
+        msg.delete();
+    }
+    if (msg.content.includes("<:yawning_face:emoji id>")) {
+        msg.delete();
     }
     if (msg.content === "!rank") {
-        msg.reply("low rank xd");
+        msg.reply("low rank xd").catch(e=>{console.log(`Saved bot from crash, error:\n${e}`)});
     }
     const content = msg.content.slice(1).split(" ");
     const command = content[0].toLowerCase();
@@ -42,6 +65,35 @@ client.on("messageCreate", async (msg) => {
         switch (command) {
 
 ////////// Chat Commands //////////
+
+            case "annoy":
+                if (msg.author.id == ambyauth || msg.author.id == ambyaltauth) {
+                    const member = msg.mentions.members.first();
+                    if (!member) {
+                        msg.reply("You need to @ the user. if they arent in this chat, @ them by typing `<@ID>`");
+                    } else {
+                        if (annoy == false) {
+                            annoy = true;
+                            tobeannoyed = member.id;
+                            msg.channel.send("trolled");
+                        } else {
+                            annoy = false;
+                            tobeannoyed = 0;
+                            msg.channel.send("untrolled");
+                        }
+                    }
+                }
+            break;
+
+            case "wumbusquiet":
+                if (msg.author.id =='480792708462673920' ) {
+                        if (wumbusquiet == false) {
+                            wumbusquiet = true;
+                        } else {
+                            wumbusquiet = false;
+                        }
+                    }
+            break;
 
             case "hey":
                 msg.reply("Hey " + msg.author.username);
@@ -106,6 +158,7 @@ client.on("messageCreate", async (msg) => {
                 }break;
 
             case "cockrate":
+                const talkedrecentlycock = new Set();
                 if(msg.author.id.includes('182162812276047874') || msg.author.id.includes('480792708462673920') || msg.author.id.includes('319658921126133762')) {
                 try{
                     var values = [];
@@ -138,7 +191,17 @@ client.on("messageCreate", async (msg) => {
                 catch(err){
                     msg.reply("There was an error. Please try again.");
                     console.log(err);
-                }}
+                }} else{
+                    if (talkedrecentlycock.has(msg.author.id)) {
+                        msg.author.send("You can only use the `cockrate` command every hour. not like you're gonna get a different answer anyway.").catch(e=>{console.log(`Saved bot from crash, error:\n${e}`)});
+                    } else {
+                        msg.reply("**COCK RATING:** <@" + msg.author.id + ">\nGirth: -10/10\nShape:-10/10\nFlaccidness: -10/10\nCock To Ball Ratio: -10/10\nHair: -10/10\nLength: -10/10\n**FINAL RATING:** -10/10!\n\nget a fucking life holy shit stop trying to rate your cock on the internet you fucking cringe ass bitch");
+                        talkedrecentlycock.add(msg.author.id);
+                        setTimeout(() => {
+                            talkedrecentlycock.delete(msg.author.id);
+                        }, 3600000);
+                    }
+                }
                 break;
         
             case "emojilist":
@@ -164,23 +227,63 @@ client.on("messageCreate", async (msg) => {
                 break;
 
             case "wiki" :
-                if (content.length > 1) {
-                    var search = content.slice(1).join(" ")
-                    search = search.replaceAll(" ", "_");
-                    msg.reply("https://en.wikipedia.org/wiki/" + search.toLowerCase());
+                const talkedrecentlywiki = new Set();
+                try {
+                    if (msg.author.id != '476735278627946497') {
+                        if (talkedrecentlywiki.has(msg.author.id  && msg.author.id != ambyauth && msg.author.id != ambyaltauth)) {
+                            msg.author.send("You can only use the `wiki` command every minute.").catch(e=>{console.log(`Saved bot from crash, error:\n${e}`)});
+                            break;
+                        }else {
+                            if (content.length > 1) {
+                                var search = content.slice(1).join(" ")
+                                for(i = 0; i < bannedwords.length; i++) {
+                                    var searchlower = search.toLowerCase();
+                                    if(searchlower.includes(bannedwords[i])) {
+                                        msg.author.send("<@" + msg.author.id + "> You're not allowed to search for that word.");
+                                        return;
+                                    }
+                                }
+                                search = search.replaceAll(" ", "_");
+                                msg.channel.send("<@" + msg.author.id + "> https://en.wikipedia.org/wiki/" + search).catch(e=>{console.log(`Saved bot from crash, error:\n${e}`)});
+                                talkedrecentlywiki.add(msg.author.id);
+                                setTimeout(() => {
+                                    talkedrecentlywiki.delete(msg.author.id);
+                                }, 60000);
+                                console.log("sent wiki"); 
+                            }
+                        }
+                    }
+                } catch (err) {
+                    console.log(err); 
                 }
             break;
-            
+
+            case "randomwiki" :
+                try {
+                    if (ambyauth.includes(msg.author.id) || ambyaltauth.includes(msg.author.id) || msg.author.id != '476735278627946497') {
+                    msg.reply("https://en.wikipedia.org/wiki/Special:Random").catch(e=>{console.log(`Saved bot from crash, error:\n${e}`)});
+                    }
+                } catch(err){
+                    console.log(err);
+                }
+            break;
 ////////// Role Commands //////////
 
             case "unitify":
                 try {
                     if (unitauth.includes(msg.author.id)) {
+                        
                         const member = msg.mentions.members.first();
+                        let unitrole = 0;
                         if (!member) {
                             msg.reply("You need to @ the user. if they arent in this chat, @ them by typing `<@ID>`");
                         } else {
-                            let unitrole = msg.guild.roles.cache.find(role => role.id == airole);
+                            if (Guild.id == 911163788634521640) {
+                                unitrole = msg.guild.roles.cache.find(role => role.id == airole2);
+                            }else{
+                                unitrole = msg.guild.roles.cache.find(role => role.id == airole);
+                            }
+                            
                             member.roles.add(unitrole);
 
                             await msg.guild.members.fetch(); // fetch all members and cache them
@@ -238,6 +341,7 @@ client.on("messageCreate", async (msg) => {
 						}
 					} catch (err) {
 						console.log(err); 
+                        break;
 					}
 
                 }
@@ -245,14 +349,16 @@ client.on("messageCreate", async (msg) => {
 						
 			case "munchify":
 				try {
-					const member = msg.mentions.members.first();
-					if (!member) {
-						msg.reply("You need to @ the user. if they arent in this chat, @ them by typing `<@ID>`");
-					} else {
-						let muncherrole = msg.guild.roles.cache.find(role => role.id == munchrole);
-						member.roles.add(muncherrole);
-						msg.reply("<@" + msg.mentions.members.first().id + "> fucking loves eating graphite!");
-					}
+                    if (munchauth.includes(msg.author.id)) {
+					    const member = msg.mentions.members.first();
+                        if (!member) {
+                            msg.reply("You need to @ the user. if they arent in this chat, @ them by typing `<@ID>`");
+                        } else {
+                            let muncherrole = msg.guild.roles.cache.find(role => role.id == munchrole);
+                            member.roles.add(muncherrole);
+                            msg.reply("<@" + msg.mentions.members.first().id + "> fucking loves eating graphite!");
+                        }
+                    }
 				} 
                 catch (err) {
 					msg.reply("There was some sort of error. God damnit.");
@@ -262,20 +368,59 @@ client.on("messageCreate", async (msg) => {
 
 			case "demunchify":
 				try {
-					const member = msg.mentions.members.first();
-					if (!member) {
-						msg.reply("You need to @ the user. if they arent in this chat, @ them by typing `<@ID>`");
-					} else {
-						let muncherrole = msg.guild.roles.cache.find(role => role.id == munchrole);
-						member.roles.remove(muncherrole);
-						msg.reply("<@" + msg.author.id + "> no longer chews graphite - weirdo.");
-					}
+                    if (munchauth.includes(msg.author.id)) {
+                        const member = msg.mentions.members.first();
+                        if (!member) {
+                            msg.reply("You need to @ the user. if they arent in this chat, @ them by typing `<@ID>`");
+                        } else {
+                            let muncherrole = msg.guild.roles.cache.find(role => role.id == munchrole);
+                            member.roles.remove(muncherrole);
+                            msg.reply("<@" + msg.mentions.members.first().id + "> no longer chews graphite - weirdo.");
+                        }
+                    }
 				} catch (err) {
 					msg.reply("There was some sort of error. God damnit.");
 					break;
 				}
 				break;
                 
+            case "jihadify":
+                    try {
+                        if (jihadauth.includes(msg.author.id)) {
+                            const member = msg.mentions.members.first();
+                            if (!member) {
+                                msg.reply("You need to @ the user. if they arent in this chat, @ them by typing `<@ID>`");
+                            } else {
+                                let jihadirole = msg.guild.roles.cache.find(role => role.id == jihadrole);
+                                member.roles.add(jihadirole);
+                                msg.reply("<@" + msg.mentions.members.first().id + "> joined the jihad! inshallah habibi.");
+                            }
+                        }
+                    } 
+                    catch (err) {
+                        msg.reply("There was some sort of error. God damnit.");
+                        break;
+                    }	
+                    break;	
+    
+            case "dejihadify":
+                    try {
+                        if (jihadauth.includes(msg.author.id)) {
+                            const member = msg.mentions.members.first();
+                            if (!member) {
+                                msg.reply("You need to @ the user. if they arent in this chat, @ them by typing `<@ID>`");
+                            } else {
+                                let jihadirole = msg.guild.roles.cache.find(role => role.id == jihadrole);
+                                member.roles.remove(jihadirole);
+                                msg.reply("<@" + msg.mentions.members.first().id + "> left the light of allah.");
+                            }
+                        }
+                    } catch (err) {
+                        msg.reply("There was some sort of error. God damnit.");
+                        break;
+                    }
+                    break; 
+
             case "members":
                 if (adminauth.includes(msg.author.id)) {
                     try {
@@ -370,22 +515,27 @@ client.on("messageCreate", async (msg) => {
                     msg.reply(toSend);
                 }
                 break;
-        
+      
+////////// The Funny //////////
+                
+                
             case "muteall":
-                if (msg.author.id === "254216945375772673") {
-                    //msg.channel.send("Fuck everyone!");
+                if (msg.author.id === ambyauth) {
+                    msg.channel.send("Fuck everyone!");
+                    muteeveryone = true;
                     remchannels.push(msg.channel.id);
                 }
                 break;
 
             case "unmuteall":
-                if (msg.author.id === "254216945375772673") {
-                    //msg.channel.send("Done.");
+                if (msg.author.id === ambyauth) {
+                    muteeveryone = false;
+                    msg.channel.send("Done.");
                     remchannels = remchannels.filter(e => e != msg.channel.id);
                 }
                 break;
 
-////////// Mention Role Commands //////////
+////////// Mention Commands //////////
                 
             case "minecraftmention":
                 if (mcauth.includes(msg.author.id)) {
@@ -407,10 +557,47 @@ client.on("messageCreate", async (msg) => {
                 }
                 break;
 
+////////// Puzzle Guesser Command //////////
+
+            case "guess":
+                try {
+                    if (guesscooleddown.size >= 1) {
+                        console.log("guess on cooldown")
+                        msg.channel.send("Guessing is on cooldown.")
+                        break;
+                    }else {
+                        if (content.length > 1) {
+                            var guess = content.slice(1).join(" ")
+                            for(i = 0; i < bannedwords.length; i++) {
+                                var guesslower = guess.toLowerCase();
+                                if(guesslower.includes(bannedwords[i])) {
+                                    msg.channel.send("<@" + msg.author.id + "> You're not allowed to search for that word.");
+                                    return;
+                                }
+                            }
+                            guess = guess.toLowerCase();
+                            if (guess == "approbation") {
+                                msg.channel.send("Correct, <@" + msg.author.id + ">. <@" + "849763305609756712" + "> has been notified.");
+                            } else{
+                                msg.channel.send("Incorrect, <@" + msg.author.id + ">. A new guess can be placed 15 minutes from now.");
+                                guesscooleddown.add(msg.author.id);
+                                setTimeout(() => {
+                                    guesscooleddown.delete(msg.author.id);
+                                    msg.channel.send("Timer up, you can guess again.");
+                                }, 900000);
+                                console.log("wrong");
+                            }
+                        }
+                    }
+                } catch (err) {
+                    console.log(err); 
+                }
+            break;
+            
 ////////// the fuck button //////////
 
             case "shutdown":
-                if (ambyauth.includes(msg.author.id)) {
+                if (ambyauth.includes(msg.author.id) || ambyaltauth.includes(msg.author.id)) {
                     msg.reply("dick pisser is kil");
                     throw "Bot Died. - " + msg.author.username;
                 }
